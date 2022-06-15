0acf11c95c914ef20aa611c9fffd837f4092a41e
Adesi <Kopietz123@gmail.com>
2022-06-16 01:56:56 +0200

    Move local version to this repo

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
+## This is a FORK!
+
+meant for private use on Firefox. I don't claim Ownership over this code and simply made it work / fixed smaller bugs for Firefox.
+
 ## Simple Auto HD
+
 Simple Auto HD quality selector for YouTube. Up to 8k/4k (60fps/50fps/48fps/30fps) supported. Theater mode.
 
 ## Latest Releases:
+
 Check out the releases page here: [https://github.com/sameernyaupane/simple-auto-hd/releases](https://github.com/sameernyaupane/simple-auto-hd/releases).
 
 ## Leave us a review!
+
 Please give us a review comment on the chrome web store if you found the extension helpful! :)
 [Chrome Web Store](https://chrome.google.com/webstore/detail/simple-auto-hd-open-sourc/jnofiabkigekemighcdaejlpgdhmbaog)
 
 ## Issues
+
 If you have any issues or suggestions please post them here:
 [https://github.com/sameernyaupane/simple-auto-hd/issues](https://github.com/sameernyaupane/simple-auto-hd/issues)
--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-(function() {
-  document.addEventListener('yt-navigate-finish', function(event) {
-    if(location.pathname === '/watch') {
+(function () {
+  document.addEventListener('yt-navigate-finish', function (event) {
+    if (location.pathname === '/watch') {
       setTimeout(() => {
         toggleTheaterMode();
         selectPreferredQuality();
@@ -8,38 +8,38 @@
     }
   });
 
-  var toggleTheaterMode = function() {
-    chrome.storage.sync.get(['theaterMode'], function(result) { 
-      updateTheaterMode(result.theaterMode); 
+  var toggleTheaterMode = function () {
+    browser.storage.sync.get(['theaterMode'], function (result) {
+      updateTheaterMode(result.theaterMode);
     });
 
-    var updateTheaterMode = function(theaterMode) {
+    var updateTheaterMode = function (theaterMode) {
       if (theaterMode === undefined) {
-        chrome.storage.sync.set({theaterMode: false}, function() {});
+        browser.storage.sync.set({ theaterMode: false }, function () { });
       }
 
       var sizeButton = document.getElementsByClassName('ytp-size-button')[0];
       var sizeButtonHasTheaterModeTitle = sizeButton.getAttribute('title') === 'Theater mode (t)';
 
-      if(theaterMode && sizeButtonHasTheaterModeTitle) {
+      if (theaterMode && sizeButtonHasTheaterModeTitle) {
         sizeButton.click();
-      } else if(!theaterMode && !sizeButtonHasTheaterModeTitle) {
+      } else if (!theaterMode && !sizeButtonHasTheaterModeTitle) {
         sizeButton.click();
       }
     }
   };
 
-  var selectPreferredQuality = function() { 
+  var selectPreferredQuality = function () {
     var preferredQuality;
     var qualitiesArray = [
-      '4320p', 
-      '4320p60', 
-      '4320p50', 
-      '4320p48', 
-      '2160p', 
-      '2160p60', 
-      '2160p50', 
-      '2160p48', 
+      '4320p',
+      '4320p60',
+      '4320p50',
+      '4320p48',
+      '2160p',
+      '2160p60',
+      '2160p50',
+      '2160p48',
       '1440p',
       '1440p60',
       '1440p50',
@@ -58,15 +58,20 @@
       '144p',
       'Auto'
     ];
+    try {
+      browser.storage.sync.get(['preferredQuality'], function (result) {
+        updateQuality(result.preferredQuality);
+      });
 
-    chrome.storage.sync.get(['preferredQuality'], function(result) { 
-      updateQuality(result.preferredQuality); 
-    });
+    } catch (error) {
+      console.log(error);
+      updateQuality();
+    }
 
-    var updateQuality = function(preferredQuality) {
+    var updateQuality = function (preferredQuality) {
       if (preferredQuality === undefined) {
         preferredQuality = 'best-available';
-        chrome.storage.sync.set({preferredQuality: preferredQuality}, function() {});
+        chrome.storage.sync.set({ preferredQuality: preferredQuality }, function () { });
       }
 
       var settingsButton = document.getElementsByClassName('ytp-settings-button')[0];
@@ -76,14 +81,14 @@
       var buttons = document.getElementsByClassName('ytp-menuitem-label');
 
       for (var i = 0; i < buttons.length; i++) {
-        if(buttons[i].innerHTML === 'Quality') {
+        if (buttons[i].innerHTML === 'Quality') {
           buttons[i].click();
         }
       }
 
       var targetItem;
 
-      if(preferredQuality === 'best-available') {
+      if (preferredQuality === 'best-available') {
         targetItem = document.querySelector('.ytp-quality-menu .ytp-menuitem-label');
       } else {
         var targetItems = document.querySelectorAll('.ytp-quality-menu .ytp-menuitem-label');
@@ -96,19 +101,29 @@
 
             var quality = potentialTargetItem.innerHTML.split(' ')[0];
 
-            if(quality === preferredQuality) {
+            if (quality === preferredQuality) {
               targetItem = potentialTargetItem;
             }
           }
 
-          if(targetItem === undefined && loopCounter < 10) {
+          if (targetItem === undefined && loopCounter < 10) {
             var key = qualitiesArray.indexOf(preferredQuality);
             preferredQuality = qualitiesArray[key + 1];
 
             loopCounter++;
-
+            //targetItem = findTargetItem();
+            //console.log(targetItem);
             return findTargetItem();
           }
+          /* loopCounter = 0;
+          if (targetItem === undefined && loopCounter < 10) {
+            var key = qualitiesArray.indexOf(preferredQuality);
+            preferredQuality = qualitiesArray[key - 1];
+
+            loopCounter++;
+
+            return findTargetItem();
+          } */
 
           return targetItem;
         }
@@ -117,14 +132,28 @@
       }
 
       targetItem.click();
+
+      var tooltip = document.querySelector(".ytp-tooltip");
+      var parent = tooltip.parentElement;
+      parent.removeChild(tooltip);
+      function ReenableTooltipAfterDelay(tooltipelement, parent) {
+        setTimeout(function () {
+          parent.appendChild(tooltipelement);
+        }, 4000);
+
+      }
+
+      ReenableTooltipAfterDelay(tooltip, parent);
+
+
     }
   };
 
-  chrome.storage.onChanged.addListener(function(changes, namespace) {
+  browser.storage.onChanged.addListener(function (changes, namespace) {
     for (key in changes) {
-      if(key === 'theaterMode') {
+      if (key === 'theaterMode') {
         toggleTheaterMode();
-      } else if(key === 'preferredQuality') {
+      } else if (key === 'preferredQuality') {
         selectPreferredQuality();
       }
     }
